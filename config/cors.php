3105730c31359c062b2e7436e5404f3d56832ed1
takuya <cdwbh564@yahoo.co.jp>
2026-02-17 16:11:39 +0900

    feat: Docker Composeのサービス名を修正し、CORS設定のオリジンURLを更新
    - サービス名をコンテナの機能に合わせて変更
    - URLを5173へ統一

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5174'], // フロントエンドの正確なURL
+    'allowed_origins' => ['http://localhost:5173'], // フロントエンドの正確なURL
 
     'allowed_origins_patterns' => [],
 
