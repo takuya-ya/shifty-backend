8bc380da0e9a3422dab32c92d3f1179d2279635c
takuya <cdwbh564@yahoo.co.jp>
2026-02-20 07:01:21 +0900

    feat:Breeze導入に伴い、 AuthControllerを削除し、関連するルートを整理

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\AuthController;
 
 Route::get('/hello', function (Request $request) {
   return response()->json(['message' => 'Hello from Laravel']);
-});
-
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->group(function () {
-  Route::get('/user', [AuthController::class, 'user']);
-  Route::post('/logout', [AuthController::class, 'logout']);
 });
