d2f394ef86ccfe21383dcb59902abe26034641a8
takuya <cdwbh564@yahoo.co.jp>
2026-02-25 18:12:29 +0900

    Revert "feat: 認証ルートをapi.phpに追加"
    This reverts commit 1825daec79a80e3d8beb7d1a9d83ec35d97a88b1.

--- routes/api.php
+++ routes/api.php
@@ -10,5 +10,3 @@ Route::get('/hello', function (Request $request) {
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
-require __DIR__ . '/auth.php';
