58c1a6b7b79becf1b337d093cf61764271a484d0
takuya <cdwbh564@yahoo.co.jp>
2026-03-05 19:00:48 +0900

    fix: UserRepository::create から name フィールドを削除
    - users テーブルから name カラムを削除したスキーマ変更に追従
    - 氏名管理を StaffProfile に一元化したため、User 生成時の name フィールドは不要となった

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -45,7 +45,6 @@ class UserRepository
     public function create(array $data): User
     {
         return User::create([
-            'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
