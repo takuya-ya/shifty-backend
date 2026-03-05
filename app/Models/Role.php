c66cb7778b72a4ca1573a446e6dae851b8aa5f79
takuya <cdwbh564@yahoo.co.jp>
2026-03-06 07:24:46 +0900

    feat: Roleモデルにロール名の定数を追加し、RoleSeederを作成してデータベースにロールをシード

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
+    /**
+     * ロール名の定数
+     */
+    public const ROLE_SUPER_ADMIN = 'Super Admin';
+    public const ROLE_STORE_ADMIN = 'Store Admin';
+    public const ROLE_STAFF = 'Staff';
+
     protected $fillable = [
         'name',
     ];
