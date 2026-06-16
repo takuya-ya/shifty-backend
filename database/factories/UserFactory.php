4f70c251b399df05013ff0f0ff641746461346db
takuya <cdwbh564@yahoo.co.jp>
2026-06-17 07:06:06 +0900

    fix: ダミーデータのpassを変更

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,7 @@ class UserFactory extends Factory
         return [
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('shifty2026'),
             'remember_token' => Str::random(10),
         ];
     }
